0bca674117ae14082dd99a05d7d80b503db57381
trapper268 <tuanhuytruong35@gmail.com>
2025-05-23 11:35:21 +0700

    fix: update CORS origin URL to include protocol

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'goldenowl-interview-fe.vercel.app',
+    origin:
+      process.env.FRONTEND_URL ?? 'https://goldenowl-interview-fe.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: false,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
